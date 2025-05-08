e9887e57079b548745b4d97b820a9a660d020d5c
estevan <estevanmartins03@gmail.com>
2025-05-08 01:01:18 -0300

    :construction: feat: add ClientDTO model

--- src/main/kotlin/com/api/sales_management/client/model/ClientDTO.kt
+++ src/main/kotlin/com/api/sales_management/client/model/ClientDTO.kt
@@ -1,3 +1,8 @@
 package com.api.sales_management.client.model
 
-data class ClientDTO()
+data class ClientDTO(
+    val id: Long? = null,
+    val nome: String,
+    val telefone: String?,
+    val endereco: String?
+)
