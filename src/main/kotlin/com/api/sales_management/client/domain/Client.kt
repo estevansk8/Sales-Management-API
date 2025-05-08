96cdc3406962905c88ba46c069b702dfda41bb90
estevan <estevanmartins03@gmail.com>
2025-05-08 01:00:16 -0300

    :construction: feat: add Client domain entity

--- src/main/kotlin/com/api/sales_management/client/domain/Client.kt
+++ src/main/kotlin/com/api/sales_management/client/domain/Client.kt
@@ -1,3 +1,23 @@
 package com.api.sales_management.client.domain
 
-data class Client()
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "cliente")
+data class Client(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val idCliente: Long = 0,
+
+    @Column(nullable = false)
+    val nome: String,
+
+    val telefone: String?,
+
+    val endereco: String?
+)
+
