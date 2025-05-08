5c561b892dd7f8275b907f156d2864a000cdd7b7
estevan <estevanmartins03@gmail.com>
2025-05-07 23:32:02 -0300

    :egg: refact: add print at start application to confirm that is running

--- src/main/kotlin/com/api/sales_management/SalesManagementApplication.kt
+++ src/main/kotlin/com/api/sales_management/SalesManagementApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class SalesManagementApplication
 
 fun main(args: Array<String>) {
+	print("Vai Corinthians!!")
 	runApplication<SalesManagementApplication>(*args)
 }
