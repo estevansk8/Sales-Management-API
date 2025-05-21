37fca94fe4fc05685a9360efb27c7f32ebaedd54
estevan <estevanmartins03@gmail.com>
2025-05-21 08:13:35 -0300

    :recycle: refact: change package structure

--- src/main/kotlin/com/api/sales_management/client/repository/ClientRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.api.sales_management.client.repository
-
-import com.api.sales_management.client.domain.Client
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ClientRepository: JpaRepository<Client, Long>
--- src/main/kotlin/com/api/sales_management/shared/dto/ApiResponseDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.api.sales_management.shared.dto
-
-data class ApiResponseDTO<T>(
-    val success: Boolean,
-    val message: String,
-    val data: T? = null,
-)
