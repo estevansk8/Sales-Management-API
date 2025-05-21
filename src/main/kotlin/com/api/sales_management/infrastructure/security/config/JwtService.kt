498b59d080b2aed9eaa4c249ed4fc75b78aac1ed
estevan <estevanmartins03@gmail.com>
2025-05-21 12:19:41 -0300

    :sparkles: feat: start JwtService

---
+++ src/main/kotlin/com/api/sales_management/infrastructure/security/config/JwtService.kt
@@ -0,0 +1,12 @@
+package com.api.sales_management.infrastructure.security.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class JwtService(
+    @Value("JWT_SECRET_BASE64") private val jwtSecret: String
+
+) {
+
+}
