85025bfd030e1fb9e989f6f590693b369e53493a
estevan <estevanmartins03@gmail.com>
2025-05-29 09:54:54 -0300

    :sparkles: feat: added ProductRepository

---
+++ src/main/kotlin/com/api/sales_management/domain/repository/ProductRepository.kt
@@ -0,0 +1,18 @@
+package com.api.sales_management.domain.repository
+
+import com.api.sales_management.domain.model.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository : JpaRepository<Product, Long> {
+    fun findByUser_Id(userId: Long): List<Product>
+
+    fun findByIdAndUser_Id(id: Long, userId: Long): Product?
+
+    fun existsByIdAndUser_Id(id: Long, userId: Long): Boolean
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.user WHERE p.id = :id")
+    fun findByIdWithUser(id: Long): Product?
+}
