80275f6f02a394f1d0a88283c385a866b74f6689
estevan <estevanmartins03@gmail.com>
2025-05-30 11:12:31 -0300

    :sparkles: feat: SaleResponseDTO created

--- src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleResponseDTO.kt
+++ src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleResponseDTO.kt
@@ -1,3 +1,18 @@
 package com.api.sales_management.application.dto.response.sale
 
-data class SaleResponseDTO()
+import com.api.sales_management.domain.model.enums.SaleStatus
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class SaleResponseDTO(
+    val id: Long,
+    val clientId: Long,
+    val clientName: String,
+    val saleDate: LocalDate,
+    var totalAmount: BigDecimal,
+    val status: SaleStatus,
+    val dueDate: LocalDate?,
+    val createdAt: LocalDateTime,
+    val items: List<SaleItemResponseDTO>
+)
