110e606669278c47cd9772437a3fa5606833cb21
estevan <estevanmartins03@gmail.com>
2025-05-30 10:18:31 -0300

    :recycle: refact: validity acess token

--- src/main/kotlin/com/api/sales_management/infrastructure/security/service/JwtService.kt
+++ src/main/kotlin/com/api/sales_management/infrastructure/security/service/JwtService.kt
@@ -15,8 +15,8 @@ class JwtService(
     @Value("\${jwt.secret}") private val jwtSecret: String
 ) {
     private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
-    private val accessTokenValidityMs = 15L * 60L * 1000L
-    val refreshTokenValidityMs = 30L * 24 * 60 * 60 * 1000L
+    private val accessTokenValidityMs = 30L * 24 * 60 * 60 * 1000L
+    val refreshTokenValidityMs = 60L * 24 * 60 * 60 * 1000L
 
     private fun generateToken(
         userId: String,
