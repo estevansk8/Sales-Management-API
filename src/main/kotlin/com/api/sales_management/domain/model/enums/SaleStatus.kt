a3aab7d72fd84d44134f4b66d9c6c7c176170978
estevan <estevanmartins03@gmail.com>
2025-05-30 10:51:40 -0300

    :sparkles: feat: SaleStatus created

--- src/main/kotlin/com/api/sales_management/domain/model/enums/SaleStatus.kt
+++ src/main/kotlin/com/api/sales_management/domain/model/enums/SaleStatus.kt
@@ -1,4 +1,11 @@
 package com.api.sales_management.domain.model.enums
 
 enum class SaleStatus {
+    PENDING,    // Pedido realizado, aguardando pagamento/processamento
+    PAID,       // Pago
+    PROCESSING, // Em processamento/separação
+    SHIPPED,    // Enviado
+    DELIVERED,  // Entregue
+    CANCELLED,  // Cancelado
+    REFUNDED    // Devolvido/Reembolsado
 }
