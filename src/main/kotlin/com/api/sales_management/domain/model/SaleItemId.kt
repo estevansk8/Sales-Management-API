c86180cb11624287d8e0ff844d2dacb36395b216
estevan <estevanmartins03@gmail.com>
2025-05-30 10:50:40 -0300

    :sparkles: feat; initial SaleItem

--- src/main/kotlin/com/api/sales_management/domain/model/SaleItemId.kt
+++ src/main/kotlin/com/api/sales_management/domain/model/SaleItemId.kt
@@ -1,4 +1,14 @@
 package com.api.sales_management.domain.model
 
-class SaleItemId {
-}
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+import java.io.Serializable
+
+@Embeddable
+data class SaleItemId(
+    @Column(name = "id_sale")
+    var saleId: Long = 0,
+
+    @Column(name = "id_product")
+    var productId: Long = 0
+) : Serializable
