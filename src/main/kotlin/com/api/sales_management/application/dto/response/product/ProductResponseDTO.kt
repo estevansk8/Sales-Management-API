9cd34c9d8fc7d1b7151de51518950960a307f04c
estevan <estevanmartins03@gmail.com>
2025-06-13 21:17:43 -0300

    :recycle: refactor: added JsonFormat to price at ProductResponseDTO

--- src/main/kotlin/com/api/sales_management/application/dto/response/product/ProductResponseDTO.kt
+++ src/main/kotlin/com/api/sales_management/application/dto/response/product/ProductResponseDTO.kt
@@ -1,11 +1,15 @@
 package com.api.sales_management.application.dto.response.product
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class ProductResponseDTO(
     val id: Long,
     val name: String,
+
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING)
     val price: BigDecimal,
+
     val stock: Int,
 )
