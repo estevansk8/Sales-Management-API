421350612cb1ec2539f76c4abbdeb923b300b1b0
estevan <estevanmartins03@gmail.com>
2025-06-13 21:19:41 -0300

    :sparkles: feat: added findByNameContainingIgnoreCase fun at ProductRepository

--- src/main/kotlin/com/api/sales_management/domain/repository/ProductRepository.kt
+++ src/main/kotlin/com/api/sales_management/domain/repository/ProductRepository.kt
@@ -13,6 +13,8 @@ interface ProductRepository : JpaRepository<Product, Long> {
 
     fun existsByIdAndUser_Id(id: Long, userId: Long): Boolean
 
+    fun findByNameContainingIgnoreCase(name: String): List<Product>
+
     @Query("SELECT p FROM Product p JOIN FETCH p.user WHERE p.id = :id")
     fun findByIdWithUser(id: Long): Product?
 }
