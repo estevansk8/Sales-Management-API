c35d7333c0c26d0ae5414898e5a97bce4ff7a8a0
estevan <estevanmartins03@gmail.com>
2025-06-18 12:35:02 -0300

    :recycle: refactor: added JsonFormat at totalAmount on SaleResponseDTO

--- src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleResponseDTO.kt
+++ src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleResponseDTO.kt
@@ -1,6 +1,7 @@
 package com.api.sales_management.application.dto.response.sale
 
 import com.api.sales_management.domain.model.enums.SaleStatus
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -10,7 +11,10 @@ data class SaleResponseDTO(
     val clientId: Long,
     val clientName: String,
     val saleDate: LocalDate,
+
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING)
     var totalAmount: BigDecimal,
+
     val status: SaleStatus,
     val dueDate: LocalDate?,
     val createdAt: LocalDateTime,
