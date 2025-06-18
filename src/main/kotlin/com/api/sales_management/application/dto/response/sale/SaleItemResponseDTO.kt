555c190ee7f5cd17ef47f49406ee8cfb361a3e4a
estevan <estevanmartins03@gmail.com>
2025-06-18 12:34:18 -0300

    :recycle: refactor: added JsonFormat at unitPrice and subtotal on SaleItemResponseDTO

--- src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleItemResponseDTO.kt
+++ src/main/kotlin/com/api/sales_management/application/dto/response/sale/SaleItemResponseDTO.kt
@@ -1,11 +1,16 @@
 package com.api.sales_management.application.dto.response.sale
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
 
 data class SaleItemResponseDTO(
     val productId: Long,
     val productName: String,
     val quantity: Int,
+
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING)
     val unitPrice: BigDecimal,
+
+    @get:JsonFormat(shape = JsonFormat.Shape.STRING)
     val subtotal: BigDecimal
 )
